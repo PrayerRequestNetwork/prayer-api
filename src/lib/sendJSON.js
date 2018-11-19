0273db58415eabe55a49b446c84d87add168fcc8
Justin Morris <justinjodymorris@gmail.com>
2018-11-19 09:25:17 -0800

    added routes for CRUD operations on user

---
+++ src/lib/sendJSON.js
@@ -0,0 +1,7 @@
+export const sendJSON = (res, data, statusCode = 200) => {
+  res.statusCode = statusCode;
+  res.statusMessage = 'OK';
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(data));
+  res.end();
+};
